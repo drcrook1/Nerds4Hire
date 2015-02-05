306ee0f9bd9674b795494b7930c2cd5785421569
Dave Crook <dacrook@microsoft.com>
2015-02-05 16:58:10 -0500

    adding all files.

---
+++ NerdsForHire/NerdsForHire.SQL/dbo/tables/Nerd.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Nerd]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Specialty] INT NOT NULL, 
+    CONSTRAINT [FK_Nerd_Specialty] FOREIGN KEY ([Specialty]) REFERENCES [dbo].[Specialty]([Id])
+)
