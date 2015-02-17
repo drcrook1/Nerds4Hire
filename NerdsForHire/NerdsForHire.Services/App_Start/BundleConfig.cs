8853a05fe53fc216e2f5c1db31400dcfd5ac17ef
Dave Crook <dacrook@microsoft.com>
2015-02-17 10:15:54 -0500

    Added Angular

--- NerdsForHire/NerdsForHire.Services/App_Start/BundleConfig.cs
+++ NerdsForHire/NerdsForHire.Services/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Optimization;
 
 namespace NerdsForHire.Services
@@ -8,21 +9,19 @@ namespace NerdsForHire.Services
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+            "~/Content/bootstrap.css",
+            "~/Content/site.css"));
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            string[] angularBundle = new string[2]{"~/scripts/vendor/angular/angular.min.js" , "~/scripts/vendor/angular/angular-ui-router.min.js"};
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(angularBundle));
+            bundles.Add(new ScriptBundle("~/bundles/Nerds4Hire")
+            .IncludeDirectory("~/scripts/controllers", "*.js")
+            .IncludeDirectory("~/scripts/factories", "*.js")
+            .Include(new string[1]{"~/scripts/app.module.js"}));
+            BundleTable.EnableOptimizations = false;
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
         }
     }
 }
