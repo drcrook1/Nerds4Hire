7892996c4f17c6a644193a62004b0001e88d49ae
drcrook1 <drcrook@outlook.com>
2015-05-06 10:53:17 -0400

    Registration WOrks

--- NerdsForHire/NerdsForHire.Services/Models/IdentityModels.cs
+++ NerdsForHire/NerdsForHire.Services/Models/IdentityModels.cs
@@ -9,7 +9,6 @@ namespace NerdsForHire.Services.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string UserName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -22,7 +21,7 @@ namespace NerdsForHire.Services.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SecurityConnection", throwIfV1Schema: false)
         {
         }
         
